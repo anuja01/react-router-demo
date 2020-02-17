4be0656ed23d6c1baf9474091b49354d9a130ac1
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2020-02-17 17:15:07 +0800

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import BasicRouting from './components/BasicRouting';
 
 export default function App() {
   return (
-    <div>
-      Main App
-    </div>
+    <BasicRouting />
   );
 }
